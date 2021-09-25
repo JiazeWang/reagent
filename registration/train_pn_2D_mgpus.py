a7ce271949dd02587fdcc79869fb9f891228be7e
jiazewang <jzwang@link.cuhkedu.hk>
2021-09-25 15:10:36 +0800

    add

--- registration/train_pn_2D_mgpus.py
+++ registration/train_pn_2D_mgpus.py
@@ -186,8 +186,8 @@ def train(agent, logger, dataset, noise_type, epochs, lr, lr_step, alpha, model_
                 'epoch': epoch,
                 'optimizer_state_dict': optimizer.state_dict()
             }
-            util_model.save(agent, f"{model_path}_mgpu_shared.zip", infos)
-            model_epoch_path = os.path.join(code_path, f"weights/shared_mgpus_pn_2d_{dataset}_{mode}_{str(epoch)}")
+            util_model.save(agent, f"{model_path}_mgpu_shared_e100.zip", infos)
+            model_epoch_path = os.path.join(code_path, f"weights/e100_shared_mgpus_pn_2d_{dataset}_{mode}_{str(epoch)}")
             util_model.save(agent, f"{model_epoch_path}.zip", infos)
         logger.dump(step=epoch)
 
@@ -259,7 +259,7 @@ if __name__ == '__main__':
 
         os.mkdir(os.path.join(code_path, "weights"))
     model_path = os.path.join(code_path, f"weights/{dataset}_{mode}")
-    logger = Logger(log_dir=os.path.join(code_path, f"logs/{dataset}/"), log_name=f"shared_mGPUs_2D_pn_{mode}",
+    logger = Logger(log_dir=os.path.join(code_path, f"logs/{dataset}/"), log_name=f"e100_shared_mGPUs_2D_pn_{mode}",
                     reset_num_timesteps=True)
     if torch.cuda.device_count() > 1:
         print("Using multiple GPUs")
@@ -270,7 +270,7 @@ if __name__ == '__main__':
 
     if args.mode == "pretrain" and dataset == "m40":
         print(f"Training: dataset '{dataset}'  - mode '{args.mode}'")
-        train(agent, logger, dataset, noise_type="clean", epochs=50, lr=1e-3, lr_step=10, alpha=0,
+        train(agent, logger, dataset, noise_type="clean", epochs=100, lr=1e-3, lr_step==20, alpha=0,
               model_path=model_path)
     else:
         if args.mode == "il":
@@ -285,17 +285,17 @@ if __name__ == '__main__':
 
         if dataset == "m40":
             print("  loading pretrained weights...")
-            if os.path.exists(os.path.join(code_path, f"weights/m40_pretrain_mgpu_shared.zip")):
-                util_model.load(agent, os.path.join(code_path, f"weights/m40_pretrain_mgpu_shared.zip"))
+            if os.path.exists(os.path.join(code_path, f"weights/m40_pretrain_mgpu_shared_e100.zip")):
+                util_model.load(agent, os.path.join(code_path, f"weights/m40_pretrain_mgpu_shared_e100.zip"))
             else:
                 raise FileNotFoundError(f"No pretrained weights found at "
                                         f"{os.path.join(code_path, f'weights/m40_pretrain.zip')}. Run with "
                                         f"'pretrain' first or download the provided weights.")
 
         noise_type = "jitter" if dataset == "m40" else "segmentation"
-        epochs = 50 if dataset == "m40" else 100
+        epochs = 100 if dataset == "m40" else 200
         lr = 1e-4 if dataset == "m40" else 1e-3
-        lr_step = 10 if dataset == "m40" else 20
+        lr_step = 20 if dataset == "m40" else 40
 
         train(agent, logger, dataset, noise_type, epochs=epochs, lr=lr, lr_step=lr_step,
               alpha=alpha, reward_mode=reward_mode, model_path=model_path)
