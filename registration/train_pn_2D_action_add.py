fc74c17103f550fab897951fde0a3daa03a25584
JiazeWang <jzwang@link.cuhk.edu.hk>
2021-10-10 03:05:41 +0800

    Update train_pn_2D_action_add.py

--- registration/train_pn_2D_action_add.py
+++ registration/train_pn_2D_action_add.py
@@ -259,7 +259,7 @@ if __name__ == '__main__':
 
         os.mkdir(os.path.join(code_path, "weights"))
     model_path = os.path.join(code_path, f"weights/{dataset}_{mode}")
-    logger = Logger(log_dir=os.path.join(code_path, f"logs/{dataset}/"), log_name=f"action_{mode}",
+    logger = Logger(log_dir=os.path.join(code_path, f"logs/{dataset}/"), log_name=f"action_e100_{mode}",
                     reset_num_timesteps=True)
     if torch.cuda.device_count() > 1:
         print("Using multiple GPUs")
